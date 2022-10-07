5d42534b44d5e9d6cbf465b673308fb346c7c46b
Samuel Afotey Laryea <afoteylaryeasamuel@gmail.com>
2022-10-07 18:53:17 +0000

    interceptor

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { throwError, Observable, retry, catchError } from 'rxjs';
 
 @Injectable({
@@ -9,7 +10,7 @@ export class AuthService {
 
   REST_API_SERVER ='http://localhost:8080/api/auth/signin';
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient, private router: Router) { }
 
   handleError(error: HttpErrorResponse) {
     let errorMessage = 'Unknown error!';
@@ -26,5 +27,17 @@ export class AuthService {
 
   login(user: any): Observable<any> {
     return this.httpClient .post<any>(this.REST_API_SERVER, user).pipe(retry(3),catchError(this.handleError));
+  }
+
+  GetToken(){
+    return localStorage.getItem('token')||'';
+   }
+
+   logOut(){
+    sessionStorage.removeItem("token")
+    sessionStorage.removeItem("username")
+    sessionStorage.removeItem("id")
+    sessionStorage.removeItem("email")
+    this.router.navigate(['login'])
   }
 }
